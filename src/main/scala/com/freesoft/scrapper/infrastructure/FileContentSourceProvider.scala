9e3efe912f76cee8ef585a4942a6ab628c15a42f
dinabogdan <bogdan.dina03@gmail.com>
2020-06-12 19:11:59 +0300

    add FileContentSourceProvider

---
+++ src/main/scala/com/freesoft/scrapper/infrastructure/FileContentSourceProvider.scala
@@ -0,0 +1,32 @@
+package com.freesoft.scrapper.infrastructure
+
+import akka.NotUsed
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Keep, Sink, Source}
+
+import scala.concurrent.Future
+
+sealed class SingleItemFilter(val item: String)
+
+case class PlacesFilter(val placeName: String) extends SingleItemFilter(placeName)
+
+case class ImmobileTypesFilter(val immobileType: String) extends SingleItemFilter(immobileType)
+
+case class SaleTypeFilter(val saleType: String) extends SingleItemFilter(saleType)
+
+class FileContentSourceProvider[+T <: SingleItemFilter](val fileName: String,
+                                                        val f: String => T)
+                                                       (implicit val materializer: Materializer) {
+  require(this.getClass.getResource(s"/$fileName") != null)
+
+
+  def items: Source[T, NotUsed] = Source.fromIterator(() => scala.io.Source.fromResource(fileName)
+    .getLines()
+    .map(item => f(item))
+  )
+
+  def count(): Future[Int] =
+    items.toMat(Sink.fold(0)(
+      (counter, _) => counter + 1)
+    )(Keep.right).run()
+}
--- src/test/scala/com/freesoft/scrapper/PlacesTest.scala
+++ src/test/scala/com/freesoft/scrapper/PlacesTest.scala
@@ -1,13 +1,13 @@
 package com.freesoft.scrapper
 
-import akka.stream.scaladsl.Source
+import com.freesoft.scrapper.infrastructure.{FileContentSourceProvider, PlacesFilter}
 import org.scalatest.freespec.AnyFreeSpec
 
 class PlacesTest extends AnyFreeSpec with AkkaSpec {
 
   "count" - {
     "should return 24 if the file provided is places.txt" in {
-      val placesFilter = new PlacesFilter("places.txt")
+      val placesFilter = new FileContentSourceProvider[PlacesFilter]("places.txt", { s => PlacesFilter(s) })
 
       val count = placesFilter.count().futureValue
 
@@ -15,12 +15,11 @@ class PlacesTest extends AnyFreeSpec with AkkaSpec {
     }
     "should throw exception if the file provided not exists" in {
       val exception = intercept[IllegalArgumentException] {
-        val placesFilter = new PlacesFilter("p.txt")
+        val placesFilter = new FileContentSourceProvider[PlacesFilter]("p.txt", { s => PlacesFilter(s) })
         placesFilter.count().futureValue
       }
 
       assert(exception.isInstanceOf[IllegalArgumentException])
     }
   }
-
 }
